5a2c821bc6fdaade3930faccd0128db2621f7c0a
kyshivers <kyshivers@yahoo.com>
2024-12-17 00:15:47 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 # Import python packages
 import streamlit as st
+import requests
 import pandas as pd
 from snowflake.snowpark.functions import col
 
@@ -38,19 +39,16 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-
-        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+        #search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
         #st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
         #st.stop()
 
         st.subheader(fruit_chosen + ' Nutrition Information')
         #New section to display smoothiefroot nutrition info
-        import requests
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-        #st.text(smoothiefroot_response)
-        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
-
-
+        
+        
     #st.write(ingredients_string)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
