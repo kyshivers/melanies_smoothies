d12c16b3a7beb4220408fb449f9777ccc0b3dbe0
kyshivers <kyshivers@yahoo.com>
2024-12-17 00:07:18 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,7 +44,10 @@ if ingredients_list:
         #st.stop()
 
         st.subheader(fruit_chosen + ' Nutrition Information')
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/")
+        #New section to display smoothiefroot nutrition info
+        import requests
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        st.text(smoothiefroot_response)
         st.stop()
         pd_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
